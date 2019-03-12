b7bb7c6400c156068741f3f6ec8fd0b4d66598aa
Richard.Cabral@verndale.com <richard.cabral@verndale.com>
2019-03-12 12:46:53 -0400

    Overall solution structure and universal configuration files added.

---
+++ Website/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Website
+{
+    public class MvcApplication : Sitecore.Web.Application
+	{
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
