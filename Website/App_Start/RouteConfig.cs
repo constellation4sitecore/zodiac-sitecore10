25bbdd170e97518c62030c16e98b546dbdad8803
SitecoreRick <sitecorerick@gmail.com>
2020-08-05 12:24:49 -0400

    Pulled in config file settings from default Sitecore 10 installation.

--- Website/App_Start/RouteConfig.cs
+++ Website/App_Start/RouteConfig.cs
@@ -3,6 +3,8 @@ using System.Web.Routing;
 
 namespace Website
 {
+	// This class is here to ensure that someone doesn't automatically add this route via a wizard and break Sitecore.
+	// You can add routes here if you need to, but you should avoid custom routes. Use Sitecore Items with Controllers instead.
 	public class RouteConfig
 	{
 		public static void RegisterRoutes(RouteCollection routes)
--- Website/Global.asax.cs
+++ Website/Global.asax.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Website
 {
-    public class MvcApplication : Sitecore.Web.Application
+	/// <summary>
+	/// Sitecore will not work without this file. You can modify the Application events if you wish, but
+	/// be sure to implement any base logic from Sitecore.Web.Application if you do so to keep Sitecore running.
+	/// </summary>
+	public class MvcApplication : Sitecore.Web.Application
 	{
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
-    }
+		protected void Application_Start()
+		{
+			// Because Zodiac uses Areas for Project-level components and Presentation from Features, you must register Areas.
+			AreaRegistration.RegisterAllAreas();
+
+			// Load RouteConfig from App_Start
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+		}
+	}
 }
