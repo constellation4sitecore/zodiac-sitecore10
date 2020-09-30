842a32ce53b0749bbcfbe6423a4a07bad83a2a1f
SitecoreRick <sitecorerick@gmail.com>
2020-09-30 16:32:45 -0400

    Starting to gather all the pieces needed to ship with a manual.

---
+++ Feature.Content/Models/CallToActionCardModel.cs
@@ -0,0 +1,16 @@
+﻿using Constellation.Foundation.ModelMapping.FieldModels;
+using System.Web;
+
+namespace Feature.Content.Models
+{
+	public class CallToActionCardModel
+	{
+		public ImageModel ImageHeader { get; set; }
+
+		public HtmlString Heading { get; set; }
+
+		public HtmlString CardText { get; set; }
+
+		public GeneralLinkModel ButtonLink { get; set; }
+	}
+}
---
+++ Feature.Content/Models/ContentSectionModel.cs
@@ -0,0 +1,21 @@
+﻿using Constellation.Foundation.ModelMapping.MappingAttributes;
+using System.Web;
+
+namespace Feature.Content.Models
+{
+	public class ContentSectionModel
+	{
+		public HtmlString Heading { get; set; }
+
+		public HtmlString Copy { get; set; }
+
+		[FieldRendererParams("class=btn btn-primary")]
+		public HtmlString TrailingLink { get; set; }
+
+		public string TrailingLinkUrl { get; set; }
+
+		public string TrailingLinkText { get; set; }
+
+		public string TrailingLinkTarget { get; set; }
+	}
+}
