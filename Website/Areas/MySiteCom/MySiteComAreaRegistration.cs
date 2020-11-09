28d56d20f74d07d045e82f722a6688f8527a67d7
SitecoreRick <sitecorerick@gmail.com>
2020-11-09 14:51:01 -0500

    More manual content but also an update for ModelMapping to fix a bug.

---
+++ Website/Areas/MySiteCom/MySiteComAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Website.Areas.MySiteCom
+{
+    public class MySiteComAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "MySiteCom";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "MySiteCom_default",
+                "MySiteCom/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
