85c4ec1f248d5b86f0745cdbb39449735632639e
SitecoreRick <sitecorerick@gmail.com>
2021-12-22 15:49:16 -0500

    Added Accordion Container and Accordion Tab. Not finished. Needs thumbnails and other details.

---
+++ Feature.Content/Models/AccordionTabModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+
+namespace Feature.Content.Models
+{
+	public class AccordionTabModel
+	{
+		public HtmlString Heading { get; set; }
+		public HtmlString Copy { get; set; }
+	}
+}
