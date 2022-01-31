62bda91280cc973f8f48031cab0246ad4e0b10e4
SitecoreRick <sitecorerick@gmail.com>
2022-01-31 16:07:26 -0500

    Started implementing Breadcrumbs

---
+++ Feature.Navigation/Controllers/BreadcrumbsController.cs
@@ -0,0 +1,21 @@
+﻿using Constellation.Feature.Navigation.Repositories;
+using Constellation.Foundation.Mvc;
+using Sitecore.Data.Items;
+
+namespace Feature.Navigation.Controllers
+{
+	public class BreadcrumbsController : ConventionController
+	{
+		public BreadcrumbsController(IViewPathResolver viewPathResolver, BreadcrumbNavigationRepository repository) : base(viewPathResolver)
+		{
+			Repository = repository;
+		}
+
+		protected BreadcrumbNavigationRepository Repository { get; }
+
+		protected override object GetModel(Item datasource, Item contextItem)
+		{
+			return Repository.GetNavigation(contextItem, Sitecore.Context.Site.SiteInfo);
+		}
+	}
+}
