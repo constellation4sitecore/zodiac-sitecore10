1e17cf63cd1b711ba144a66e70f3ecf69705691e
SitecoreRick <sitecorerick@gmail.com>
2022-02-18 14:46:09 -0500

    Lots of fixes for Primary Content. Changes to Alerts.

--- Feature.Alerts/Models/AlertTypeModel.cs
+++ Feature.Alerts/Models/AlertTypeModel.cs
@@ -1,14 +1,9 @@
 ﻿using Constellation.Foundation.ModelMapping.MappingAttributes;
-using Sitecore.Data;
 
 namespace Feature.Alerts.Models
 {
 	public class AlertTypeModel
 	{
-		public ID ID { get; set; }
-
-		public string DisplayName { get; set; }
-
 		[RawValueOnly]
 		public string CssClass { get; set; }
 
