a48fe11ff3133fd206e7d04286dd366efd4b461e
SitecoreRick <sitecorerick@gmail.com>
2022-03-28 10:22:36 -0400

    Wiring up CallToActionCard Rows

---
+++ Feature.RelatedContent/Controllers/CallToActionCardRowController.cs
@@ -0,0 +1,14 @@
+﻿using Constellation.Foundation.ModelMapping;
+using Constellation.Foundation.Mvc;
+using Constellation.Foundation.Mvc.Patterns.Controllers;
+using Feature.RelatedContent.Models;
+
+namespace Feature.RelatedContent.Controllers
+{
+	public class CallToActionCardRowController : DatasourceRenderingController<CallToActionCardRowModel>
+	{
+		public CallToActionCardRowController(IViewPathResolver viewPathResolver, IModelMapper modelMapper) : base(viewPathResolver, modelMapper)
+		{
+		}
+	}
+}
---
+++ Feature.RelatedContent/Models/CallToActionCardRowModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+
+namespace Feature.RelatedContent.Models
+{
+	public class CallToActionCardRowModel
+	{
+		public HtmlString Heading { get; set; }
+
+		public HtmlString Copy { get; set; }
+
+		public HtmlString TrailingCopy { get; set; }
+	}
+}
