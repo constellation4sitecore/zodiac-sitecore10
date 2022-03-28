fc897f733714db769433f6ffe71b7d1577b1196a
SitecoreRick <sitecorerick@gmail.com>
2022-03-28 16:54:23 -0400

    Call to Action Card Rows and Cards added, not tested yet.

---
+++ Feature.RelatedContent/Controllers/CallToActionCardController.cs
@@ -0,0 +1,14 @@
+﻿using Constellation.Foundation.ModelMapping;
+using Constellation.Foundation.Mvc;
+using Constellation.Foundation.Mvc.Patterns.Controllers;
+using Feature.RelatedContent.Models;
+
+namespace Feature.RelatedContent.Controllers
+{
+	public class CallToActionCardController : DatasourceRenderingController<CallToActionCardModel>
+	{
+		public CallToActionCardController(IViewPathResolver viewPathResolver, IModelMapper modelMapper) : base(viewPathResolver, modelMapper)
+		{
+		}
+	}
+}
---
+++ Feature.RelatedContent/Models/CallToActionCardModel.cs
@@ -0,0 +1,16 @@
+﻿using Constellation.Foundation.ModelMapping.FieldModels;
+using System.Web;
+
+namespace Feature.RelatedContent.Models
+{
+	public class CallToActionCardModel
+	{
+		public ImageModel ThumbnailImage { get; set; }
+
+		public HtmlString Heading { get; set; }
+
+		public HtmlString Copy { get; set; }
+
+		public GeneralLinkModel Link { get; set; }
+	}
+}
