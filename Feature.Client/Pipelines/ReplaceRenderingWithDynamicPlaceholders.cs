efc963e2eabf87b11945c82078bdd1cbc49e5e4f
SitecoreRick <sitecorerick@gmail.com>
2022-04-15 14:28:11 -0400

    Added a fix that allows dynamic placeholders to keep their assigned renderings when you switch between compatible renderings.

---
+++ Feature.Client/Pipelines/ReplaceRenderingWithDynamicPlaceholders.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Layouts;
+using Sitecore.Pipelines.ExecutePageEditorAction;
+
+namespace Feature.Client.Pipelines
+{
+	public class ReplaceRenderingWithDynamicPlaceholders : ReplaceRendering
+	{
+		protected override RenderingDefinition DoReplaceRendering(
+			RenderingDefinition sourceRendering,
+			Item targetRenderingItem,
+			DeviceDefinition device)
+		{
+			// Preserve the UID of the original rendering so that dynamic placeholders will continue to work
+			var rendering = base.DoReplaceRendering(sourceRendering, targetRenderingItem, device);
+			rendering.UniqueId = sourceRendering.UniqueId;
+			return rendering;
+		}
+	}
+}
