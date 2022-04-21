0dc72587a70f28ec0de48f68ded28a7f82032afd
SitecoreRick <sitecorerick@gmail.com>
2022-04-21 15:08:46 -0400

    Changed from an XP config to an XM config to help with compatibility with XM Cloud. Fixed a number of issues that were incompatibilities with Horizon. More remain.

--- Feature.Alerts/Controllers/JsonAlertGroupController.cs
+++ Feature.Alerts/Controllers/JsonAlertGroupController.cs
@@ -11,8 +11,14 @@ namespace Feature.Alerts.Controllers
 
 		protected AlertRepository Repository { get; }
 
-		public JsonResult Index()
+		public ActionResult Index()
 		{
+			// Measure to prevent this controller from crashing when accessed out of context.
+			if (Sitecore.Context.Database == null || Sitecore.Context.Language == null || Sitecore.Context.Site == null)
+			{
+				return new EmptyResult();
+			}
+
 			var model = Repository.GetAlerts(Sitecore.Context.Database, Sitecore.Context.Language, Sitecore.Context.Site.SiteInfo);
 
 			return Json(model, JsonRequestBehavior.AllowGet);
